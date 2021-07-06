91fb384f62a4278a3f1cb4db2b8b80e444bff6d3
Fernando Ramirez <ferjaviramirez@gmail.com>
2021-07-06 19:31:01 +0200

    chore(components): add new components

--- src/app/froged/froged.module.ts
+++ src/app/froged/froged.module.ts
@@ -2,19 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
 import { MaterialModule } from '../material/material.module';
+import { PeopleComponent } from './pages/people/people.component';
+import { StarshipComponent } from './pages/starship/starship.component';
+import { PlanetsComponent } from './pages/planets/planets.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    PeopleComponent,
+    StarshipComponent,
+    PlanetsComponent
   ],
   imports: [
     CommonModule,
     MaterialModule
   ],
   exports:[
-    HomeComponent
+    HomeComponent,
+    PeopleComponent,
+    StarshipComponent,
+    PlanetsComponent
   ]
 })
 export class FrogedModule { }
