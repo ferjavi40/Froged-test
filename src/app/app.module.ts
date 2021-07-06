fda8a1ed8ec26a2a53e6eab9383ad97a04c49f98
Fernando Ramirez <ferjaviramirez@gmail.com>
2021-07-06 15:37:42 +0200

    chore(material): add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { FrogedModule } from './froged/froged.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,9 @@ import { FrogedModule } from './froged/froged.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FrogedModule
+    FrogedModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
