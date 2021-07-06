49e2ca88207a6b0169c36edb7a22da3bf6b6f632
Fernando Ramirez <ferjaviramirez@gmail.com>
2021-07-06 15:22:06 +0200

    chore(modules): add new modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FrogedModule } from './froged/froged.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FrogedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
