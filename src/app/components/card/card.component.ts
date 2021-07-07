8f05291aa378d54e0fdb6257e6f5fa1aa470b2d1
Fernando Ramirez <ferjaviramirez@gmail.com>
2021-07-07 18:00:28 +0200

    feature(card): add card component to each page

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'ui-card',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+  label1:string= "action 1";
+  label2:string= "action 2";
+
+  @Input() data: string[];
 
   constructor() { }
 
