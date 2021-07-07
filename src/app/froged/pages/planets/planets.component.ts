6a89c964daa1d0120a8fb07effa170ccf78f8036
Fernando Ramirez <ferjaviramirez@gmail.com>
2021-07-07 14:50:07 +0200

    feature(planets): planets service working

--- src/app/froged/pages/planets/planets.component.ts
+++ src/app/froged/pages/planets/planets.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PlanetsInterface } from '../../interfaces/planet.interface';
+import { FrogedService } from '../../services/froged.service';
+
+
+
 @Component({
   selector: 'app-planets',
   templateUrl: './planets.component.html',
@@ -7,9 +12,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlanetsComponent implements OnInit {
 
-  constructor() { }
+  planets: PlanetsInterface [] = [];
+
+  constructor(private _frogedService:FrogedService) { }
 
   ngOnInit(): void {
+
+    this._frogedService.getPlanets()
+        .subscribe((data:PlanetsInterface[])=>{
+          this.planets = data['results'];
+          console.log(this.planets)
+        })
   }
+
+  
 
 }
